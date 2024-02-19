a7d4187513ac56c595bce8af2a821b9074040828
agentx3 <thepuzzlegang.agent@gmail.com>
2024-02-18 16:42:25 -0800

    Update trial process to reflect theoretical probabilities better

--- src/hand.rs
+++ src/hand.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::{
     card::Suit::{self, Clubs, Diamonds, Hearts, Spades, Wild, NONE},
@@ -54,10 +54,9 @@ fn create_hand_with_subbed_wilds(cards: &Vec<Card>) -> Hand {
 }
 
 impl Hand {
-    // Check all hands in one pass for performance
+    // Check all hands in one pass (or minimal passes) for performance
     pub fn evaluate_poker_hands(&mut self) -> HashMap<PokerHand, u32> {
         let mut rank_map = HashMap::new();
-        let mut suit_map = HashMap::new();
 
         let mut hand_map = HashMap::new();
 
@@ -70,27 +69,26 @@ impl Hand {
         // Variable to help with straights
         let mut curr_straight_streak = 1;
         let mut last_straight_rank: Rank = Rank::NONE;
+        let mut potential_straight_suits: HashSet<Suit> = HashSet::new();
+        let mut ace_suits_for_straight_flush: HashSet<Suit> = HashSet::new();
+        let mut primed_for_low_ace_straight = false;
 
         let mut last_suit: Suit = NONE;
         let mut suit_streak = 0;
 
-        // Sort the wild cards
+        // Used to distinguish flush from five-flush, only used in first loop
+        let mut last_rank: Rank = Rank::NONE;
+        let mut rank_streak = 0;
 
-        // TODO: Make this more efficient
+        // TODO: Make this more efficient, maybe we can avoid this loop?
         // Just need to get something working for now
+        // Sort the wild cards
         let mut hand_with_subbed_wilds = create_hand_with_subbed_wilds(&self.cards);
         hand_with_subbed_wilds
             .cards
             .sort_by(|a, b| a.suit.cmp(&b.suit).then(a.rank.cmp(&b.rank)));
 
-        let mut primed_for_low_ace_straight = false;
-        // Used for the loop after this one
-        let mut has_ace = false;
-
         for card in hand_with_subbed_wilds.cards.iter() {
-            if card.rank == Rank::Ace {
-                has_ace = true;
-            }
             if card.suit == last_suit || (card.suit == Wild && last_suit != NONE) {
                 suit_streak += 1;
             } else {
@@ -101,6 +99,13 @@ impl Hand {
                 // Reset the straight streak too, since we check for
                 // regular straights after this loop
                 curr_straight_streak = 1;
+
+                // Reset rank streak since we mainly care about suits here
+                if card.rank == last_rank {
+                    rank_streak += 1;
+                } else {
+                    rank_streak = 1;
+                }
             }
 
             if last_straight_rank == card.rank {
@@ -114,21 +119,32 @@ impl Hand {
             // We have a 2-5 straight right now
             if card.rank == Rank::Five && curr_straight_streak == 4 {
                 primed_for_low_ace_straight = true;
+                ace_suits_for_straight_flush.insert(card.suit);
             }
-            if suit_streak >= 5 && suit_streak > curr_straight_streak {
+            if suit_streak >= 5 && suit_streak > curr_straight_streak && rank_streak < 5 {
                 hand_map.insert(PokerHand::Flush, 1);
+            }
+            if suit_streak >= 5 && rank_streak >= 5 {
+                hand_map.insert(PokerHand::FlushFive, 1);
             }
             if curr_straight_streak >= 5 && suit_streak >= 5 {
                 hand_map.insert(PokerHand::StraightFlush, 1);
-            } else if primed_for_low_ace_straight && card.rank == Rank::Ace {
+            } else if primed_for_low_ace_straight
+                && card.rank == Rank::Ace
+                && ace_suits_for_straight_flush.contains(&card.suit)
+            {
                 hand_map.insert(PokerHand::StraightFlush, 1);
             }
             last_straight_rank = card.rank;
             last_suit = card.suit;
+            last_rank = card.rank;
         }
         // Reset for the next loop
         last_suit = NONE;
         last_straight_rank = Rank::NONE;
+        primed_for_low_ace_straight = false;
+        ace_suits_for_straight_flush.clear();
+
         // Sort the cards by rank
         self.cards
             .sort_by(|a, b| a.rank.cmp(&b.rank).then(a.suit.cmp(&b.suit)));
@@ -136,8 +152,6 @@ impl Hand {
         for (i, card) in self.cards.iter().enumerate() {
             let rank_count = rank_map.entry(card.rank).or_insert(0);
             *rank_count += 1;
-            let suit_count = suit_map.entry(card.suit).or_insert(0);
-            *suit_count += 1;
 
             if card.suit == last_suit || (card.suit == Wild && last_suit != NONE) {
                 suit_streak += 1;
@@ -193,26 +207,80 @@ impl Hand {
                     }
                 }
             };
+            // Don't look for more straights if we already have one
+            if hand_map.contains_key(&PokerHand::Straight) {
+                last_suit = card.suit;
+                continue;
+            }
+
+            // We have an ace, we need to check for a low ace straight
+            if card.rank == Rank::Ace && primed_for_low_ace_straight {
+                // Need to make sure our ace doesn't force a straight flush
+                let mut force_straight_flush = false;
+                for suit in ace_suits_for_straight_flush.iter() {
+                    if card.suit == *suit {
+                        force_straight_flush = true;
+                    }
+                }
+                if !force_straight_flush {
+                    hand_map.insert(PokerHand::Straight, 1);
+                }
+            }
+
             // Track straights
             if card.rank == last_straight_rank.next() {
                 curr_straight_streak += 1;
                 last_straight_rank = card.rank;
+                potential_straight_suits.insert(card.suit);
             } else if card.rank == last_straight_rank {
-                // Do nothing
+                // suit alternative
+                potential_straight_suits.insert(card.suit);
             } else {
-                // The card is not the next in the straight
+                // The card is not the next in the straight reset
                 curr_straight_streak = 1;
                 last_straight_rank = card.rank;
+                potential_straight_suits.clear();
             }
-            if curr_straight_streak == 4 && has_ace && card.rank == Rank::Five {
-                hand_map.insert(PokerHand::Straight, 1);
+
+            if curr_straight_streak == 4 && card.rank == Rank::Five {
+                // We have a 2-5 straight
+                primed_for_low_ace_straight = true;
+
+                // We must not match the suit with our ace
+                match potential_straight_suits.len() {
+                    // Just cycles once
+                    1 => {
+                        for suit in potential_straight_suits.iter() {
+                            ace_suits_for_straight_flush.insert(*suit);
+                            ace_suits_for_straight_flush.insert(Wild);
+                        }
+                    }
+                    // Need to check if the second suit is wild
+                    2 => {
+                        let mut non_wild_suit: Suit = NONE;
+                        let mut has_wild: bool = false;
+                        for suit in potential_straight_suits.iter() {
+                            if *suit != Wild {
+                                non_wild_suit = *suit;
+                            } else {
+                                has_wild = true;
+                            }
+                        }
+                        if has_wild {
+                            ace_suits_for_straight_flush.insert(non_wild_suit);
+                            ace_suits_for_straight_flush.insert(Wild);
+                        }
+                    }
+                    _ => {}
+                }
             }
             if curr_straight_streak == 5 {
                 // Already handled straight flushes
-                if suit_streak < 5 {
+                if potential_straight_suits.len() > 1 {
                     hand_map.insert(PokerHand::Straight, 1);
                 }
             }
+
             last_suit = card.suit;
         }
         // Check for full house
@@ -231,6 +299,12 @@ impl Hand {
                 }
             }
         }
+        // if hand_map.contains_key(&PokerHand::StraightFlush) {
+        //     println!("$$StraightFlush$$ in hand {:#?}", self.cards);
+        // }
+        // if hand_map.contains_key(&PokerHand::Flush) {
+        //     println!("$$Flush$$ in hand {:#?}", hand_with_subbed_wilds.cards);
+        // }
         hand_map
     }
 }
@@ -328,6 +402,48 @@ mod tests {
             PokerHand::Flush => 1,
             PokerHand::Straight => 1,
             PokerHand::FullHouse => 1,
+        };
+        test_hand_correctness(cards, &expected);
+    }
+    #[test]
+    fn it_evaluates_hand_correctly_3() {
+        let cards: Vec<Card>;
+
+        #[rustfmt::skip]
+        {
+            cards = vec![
+                Card { rank: Rank::Two  , suit: Clubs    },
+                Card { rank: Rank::Five , suit: Clubs    },
+                Card { rank: Rank::Three, suit: Clubs    },
+                Card { rank: Rank::Four , suit: Clubs    },
+                Card { rank: Rank::Four , suit: Hearts   },
+                Card { rank: Rank::Ace  , suit: Diamonds },
+            ];
+        }
+
+        let expected: HashMap<PokerHand, u32> = hash_map! {
+            PokerHand::Pair => 1,
+            PokerHand::Straight => 1,
+        };
+        test_hand_correctness(cards, &expected);
+    }
+    #[test]
+    fn it_evaluates_hand_correctly_4() {
+        let cards: Vec<Card>;
+
+        #[rustfmt::skip]
+        {
+            cards = vec![
+                Card { rank: Rank::Two  , suit: Clubs    },
+                Card { rank: Rank::Five , suit: Diamonds },
+                Card { rank: Rank::Three, suit: Diamonds },
+                Card { rank: Rank::Four , suit: Diamonds },
+                Card { rank: Rank::Ace  , suit: Diamonds },
+            ];
+        }
+
+        let expected: HashMap<PokerHand, u32> = hash_map! {
+            PokerHand::Straight => 1,
         };
         test_hand_correctness(cards, &expected);
     }
